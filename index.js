195037399430cdfc39e0991dcbb2246ade708a92
Saveleva Kristina <savelevakristina1995@yandex.ru>
2022-01-12 15:56:15 +0300

    test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const containerElement = document.querySelector('.container');
 
-document.addEventListener('keyup', event => {
-    if (event.code === 'Space') {
-      containerElement.remove();
-    }
-  });
+document.addEventListener('click', () => {
+  containerElement.classList.add('hidden');
+});
 
-containerElement.addEventListener('click', () => {
-    containerElement.classList.add('hidden');
+document.addEventListener('keyup', event => {
+  if (event.code === 'Space') {
+  containerElement.remove();
+  }
 });
